da38d5bf16b4037238b05e238d261f86e551baeb
qx517971976 <857917113@qq.com>
2017-11-26 00:03:58 +0800

    ff

--- lesson1/input.py
+++ lesson1/input.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
 import tab
 
+#name = raw_input('What is your name?')
+#age = int(raw_input('How old are you?'))
+
+"""
 for i in range(3):
     name = raw_input('What is your name?').strip()
     if len(name) == 0:
-#        print "name is not null!!!"
         continue
+    break
+"""
+
+"""
+if age <= 18:
+    print "你太年轻了"
+else:
+    print "我叫%s，今年%d岁" %(name,age)
+"""
+
+while True:
+    name = raw_input('What is your name?').strip()
+    age = int(raw_input('How old are you?'))
+    if len(name) or len(str(age)) == 0:
+        continue
+    else:
+        print "我叫%s" % name
+        print "今年%d岁" % age
     break
