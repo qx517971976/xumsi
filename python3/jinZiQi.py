966de4831faaf93d718211cd3df9c5ecfdc7332c
qx517971976 <857917113@qq.com>
2018-01-16 23:40:48 +0800

    ddd

---
+++ python3/jinZiQi.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+打印实现如下的图形，其中空白的9个位置正好对应9个棋子的键，只要将空值替换为O和X就相当于是完成了一盘棋
+ | | 
+-+-+-
+ | | 
+-+-+-
+ | | 
+
+'''
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+printBoard(theBoard)
